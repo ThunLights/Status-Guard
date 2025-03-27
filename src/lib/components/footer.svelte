38c79d1ca47dbbcd08e867f8a4eba59008fa3e4f
ROBOT <tkata12@outlook.jp>
2025-03-27 11:56:37 +0900

    add footer.svelte

---
+++ src/lib/components/footer.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { repoUrl } from "$lib/constants";
+</script>
+
+<footer>
+	<p>Powerd by <a href={repoUrl} target="_blank" rel="noopener noreferrer">Status Guard</a></p>
+</footer>
+
+<style>
+	footer {
+		background-color: #212227;
+	}
+	p {
+		padding: 3px 5px;
+		color: white;
+	}
+	a {
+		color: #58d4e4;
+	}
+</style>
