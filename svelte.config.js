9ff11d28558470a8029bcbc185d468a0f5d3c673
ROBOT <tkata12@outlook.jp>
2025-03-27 11:38:35 +0900

    Compatible with multiple adapters

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,22 @@
-import adapter from "@sveltejs/adapter-cloudflare-workers";
+import nodeAdapter from "@sveltejs/adapter-node";
+import cfWorkersAdapter from "@sveltejs/adapter-cloudflare-workers";
+import vercelAdapter from "@sveltejs/adapter-vercel";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+
+const adapter = (() => {
+    const adapterType = process.env.ADAPTER_TYPE;
+    if (adapterType === "node") {
+        return nodeAdapter();
+    }
+    if (adapterType === "vercel") {
+        return vercelAdapter();
+    }
+    return cfWorkersAdapter();
+})();
 
 const config = {
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: { adapter }
 };
 
 export default config;
