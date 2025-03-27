c80cb65d0305d9c9244c8dc12fdc7d6143c289f3
ROBOT <tkata12@outlook.jp>
2025-03-27 18:43:41 +0900

    add header component

---
+++ src/lib/components/header.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import "@fontsource/dela-gothic-one";
+</script>
+
+<header>
+	<p class="title"><a href="/">Status Guard</a></p>
+</header>
+
+<style>
+	header {
+		overflow: hidden;
+		position: sticky;
+		top: 0;
+		left: 0;
+		width: 100%;
+		background-color: rgb(44, 44, 44);
+	}
+	.title {
+		font-family: "Dela Gothic One", system-ui;
+		padding-left: 3px;
+		font-size: 26px;
+		font-family: 600;
+	}
+	p {
+		padding: 10px;
+	}
+</style>
