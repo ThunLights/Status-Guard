66ad00cc77cec721a11a33e99dccf32807f86c00
ROBOT <tkata12@outlook.jp>
2025-03-27 18:50:36 +0900

    add routes/+error.svelte

---
+++ src/routes/+error.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import Meta from "$lib/components/meta.svelte";
+	import Header from "$lib/components/header.svelte";
+	import Footer from "$lib/components/footer.svelte";
+
+	const title = "Page Not Found";
+</script>
+
+<Meta {title} />
+
+<Header />
+<main>
+    <p class="title text-center text-6xl">404</p>
+    <div>
+        <p class="text-center">Page not found.</p>
+    </div>
+</main>
+<Footer />
+
+<style>
+	main {
+		overflow: hidden;
+        min-height: 93vh;
+	}
+</style>
