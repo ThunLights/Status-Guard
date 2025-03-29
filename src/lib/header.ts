dc407ebc362bb45dcc3d5733a8e8614723fc1370
ROBOT <tkata12@outlook.jp>
2025-03-29 21:30:53 +0900

    add lib/header.ts

---
+++ src/lib/header.ts
@@ -0,0 +1,13 @@
+import { z } from "zod";
+
+export class Header {
+    public static readonly headerSchema = z.record(z.string(), z.string());
+
+    public static parse(data: unknown): z.infer<typeof this.headerSchema> {
+        try {
+            return this.headerSchema.parse(data);
+        } catch {
+            return {};
+        }
+    }
+}
