bc4afe4908ff480bf4cbeafcea25e90ad12d55a6
ROBOT <tkata12@outlook.jp>
2025-03-29 22:07:34 +0900

    ran formatting

--- src/lib/header.ts
+++ src/lib/header.ts
@@ -1,13 +1,13 @@
 import { z } from "zod";
 
 export class Header {
-    public static readonly headerSchema = z.record(z.string(), z.string());
+	public static readonly headerSchema = z.record(z.string(), z.string());
 
-    public static parse(data: unknown): z.infer<typeof this.headerSchema> {
-        try {
-            return this.headerSchema.parse(data);
-        } catch {
-            return {};
-        }
-    }
+	public static parse(data: unknown): z.infer<typeof this.headerSchema> {
+		try {
+			return this.headerSchema.parse(data);
+		} catch {
+			return {};
+		}
+	}
 }
