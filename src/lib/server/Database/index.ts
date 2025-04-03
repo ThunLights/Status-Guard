61c8c8a36b4d1b609d803e38de2f5a425de847b1
ROBOT <tkata12@outlook.jp>
2025-04-03 21:53:43 +0900

    fix: change postgres to prisma accelerate

--- src/lib/server/Database/index.ts
+++ src/lib/server/Database/index.ts
@@ -1,10 +1,12 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
 import { Website } from "./Database.website";
 import { Api } from "./Database.api";
 import { Status } from "./Database.status";
+import { DATABASE_URL } from "$env/static/private";
 
-export const _prisma = new PrismaClient();
+export const _prisma = new PrismaClient({ datasourceUrl: DATABASE_URL }).$extends(withAccelerate());
 
 export class Database {
 	public readonly website = new Website();
