12ae1132c77f64d9f2121020591e2a344e3b0e50
ROBOT <tkata12@outlook.jp>
2025-04-05 00:05:44 +0900

    remove: adapter-vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,5 @@
 import nodeAdapter from "@sveltejs/adapter-node";
 import cfAdapter from "@sveltejs/adapter-cloudflare-workers";
-import vercelAdapter from "@sveltejs/adapter-vercel";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 const adapter = (() => {
@@ -8,11 +7,6 @@ const adapter = (() => {
 	if (adapterType === "cf") {
 		return cfAdapter({
 			config: "wrangler.json"
-		});
-	}
-	if (adapterType === "vercel") {
-		return vercelAdapter({
-			runtime: "nodejs20.x"
 		});
 	}
 	return nodeAdapter({
